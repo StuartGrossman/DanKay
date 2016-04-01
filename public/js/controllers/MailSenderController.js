9afeb13c613079d333fd8f172b0f11fbbf085878
Stuart Grossman <grossman.stuart1@gmail.com>
2016-04-01 13:50:22 -0700

    changing home page pictures

--- public/js/controllers/MailSenderController.js
+++ public/js/controllers/MailSenderController.js
@@ -6,11 +6,17 @@
 	function MailSenderController(HomeFactory){
 		var vm = this;
 		vm.mail = {};
+		var success;
+		vm.done = false;
 		vm.sendMail = function(){
 			console.log(vm.mail);
 			HomeFactory.MailSender(vm.mail).then(function(res){
 				console.log('trying to send mail')
 				console.log(res)
+				success = res; 
+				if(success = 'Email was sent!'){
+					vm.done = true;
+				}
 			})
 		}
 
